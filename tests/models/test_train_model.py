51d2dbe3988474d996ac57e1277f39eaa36e5d7e
dnsosa <dansosa19@gmail.com>
2020-07-20 13:07:14 -0700

    Added a period....

--- tests/models/test_train_model.py
+++ tests/models/test_train_model.py
@@ -63,5 +63,5 @@ class TestTrainModel(unittest.TestCase):
         pass
 
     def tearDown(self):
-        """Clean-up after all tests have run"""
+        """Clean-up after all tests have run."""
         shutil.rmtree(self.out_dir)
