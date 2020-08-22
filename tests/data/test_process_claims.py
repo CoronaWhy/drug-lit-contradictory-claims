3859f427dcf607492af07084c632453d270d56aa
itsmemala <itsmemala@users.noreply.github.com>
2020-08-22 12:16:02 +0530

    Update test_process_claims.py

--- tests/data/test_process_claims.py
+++ tests/data/test_process_claims.py
@@ -6,7 +6,7 @@ import unittest
 
 import pandas as pd
 from contradictory_claims.data.process_claims import add_cord_metadata,\
-     split_papers_on_claim_presence, tokenize_section_text
+    split_papers_on_claim_presence, tokenize_section_text
 # initialize_nlp, pair_similar_claims\
 
 from .constants import sample_metadata_path, sample_no_claims_df_path,\
@@ -30,7 +30,7 @@ class TestProcessClaims(unittest.TestCase):
 
     def test_1_split_papers_on_claim_presence(self):
         """Test that papers are split correctly based on claim presence."""
-		claims_df = pd.read_csv(sample_raw_claims_df_path)
+        claims_df = pd.read_csv(sample_raw_claims_df_path)
         claims_data, no_claims_data\
             = split_papers_on_claim_presence(claims_df)
         self.assertEqual(len(claims_data), 12)
@@ -38,7 +38,7 @@ class TestProcessClaims(unittest.TestCase):
 
     def test_2_tokenize_section_text(self):
         """Test that section text is tokenized properly."""
-		no_claims_data = pd.read_csv(sample_no_claims_df_path)
+        no_claims_data = pd.read_csv(sample_no_claims_df_path)
         tok_no_claims_data = tokenize_section_text(no_claims_data)
         self.assertEqual(len(tok_no_claims_data), 15)
 
@@ -56,6 +56,6 @@ class TestProcessClaims(unittest.TestCase):
 
     def test_5_add_cord_metadata(self):
         """Test that input CORD metadata is added properly."""
-		claims_paired_df = pd.read_csv(sample_paired_claims_df_path)
-        claims_paired_meta_df = add_cord_metadata(self.claims_paired_df, sample_metadata_path)
+        claims_paired_df = pd.read_csv(sample_paired_claims_df_path)
+        claims_paired_meta_df = add_cord_metadata(claims_paired_df, sample_metadata_path)
         self.assertEqual(len(claims_paired_meta_df.columns), 11)
