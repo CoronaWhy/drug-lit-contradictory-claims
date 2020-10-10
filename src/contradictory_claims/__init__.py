fd6391aef60b765b3c69936a54f546072adf04ce
97harsh <harsh25011997@hotmail.com>
2020-10-10 16:06:08 +0530

    merge conflicts

--- src/contradictory_claims/__init__.py
+++ src/contradictory_claims/__init__.py
@@ -6,9 +6,6 @@ from .data.make_dataset import *  # noqa:F401,F403
 # from .extract_claims import  # noqa:F401,F403
 #  from .models import train_model  # noqa:F401
 from .models.train_model import *  # noqa:F401,F403
-<<<<<<< HEAD
 from .models.sbert_models import train_sbert_model  # noqa:F401
-=======
 from .models.evaluate_model import *  # noqa:F401,F403
 from .models.analysis_utils import *  # noqa:F401,F403
->>>>>>> 6778315ec2b65c5bf11704d36a8b1e3b752fdf2a
