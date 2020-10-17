f2185d093967a4fb9df1f4fa6c94eec1043abbfc
97harsh <harsh25011997@hotmail.com>
2020-10-17 20:06:02 +0530

    flake 8 issues

--- src/contradictory_claims/models/evaluate_model.py
+++ src/contradictory_claims/models/evaluate_model.py
@@ -8,8 +8,8 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from contradictory_claims.models.train_model import regular_encode
 from contradictory_claims.models.dataloader import ClassifierDataset
+from contradictory_claims.models.train_model import regular_encode
 from sklearn.metrics import accuracy_score, auc, confusion_matrix, f1_score, precision_score, recall_score, roc_curve
 from sklearn.preprocessing import label_binarize
 from transformers import AutoTokenizer
