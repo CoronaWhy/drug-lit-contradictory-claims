Fixed gradient clipping and weighting issues from before

src/contradictory_claims/models/bluebert_train_model.py
@@ -170,8 +170,8 @@ def bluebert_train_model(model,
     elif criterion == 'crossentropy':
         torch_criterion = torch.nn.CrossEntropyLoss()
     if optimizer is None:
-        # NOTE: using clip value
-        optimizer = AdamW(model.parameters(), lr=learning_rate, eps=1e-8, clipvalue=0.5)
+        # NOTE: using clip value, default = 1.0
+        optimizer = AdamW(model.parameters(), lr=learning_rate, eps=1e-8)
 
     # Set the seed value all over the place to make this reproducible.
     np.random.seed(seed)
@@ -236,17 +236,16 @@ def bluebert_train_model(model,
     return model, loss_values
 
 
-def get_class_weights(target_list: list):
+def get_class_weights(target_list: np.array):
     """Return the class weights to tackle skewness in data while training.
-    :param target_list: list indicating class membership for calculating imbalance
+    :param target_list: numpy array list indicating binary-encoded class membership for calculating imbalance
     :return: list of weights
     """
-    count_dict = Counter(target_list)
-    class_count = [count_dict[i] for i in range(3)]
-    class_weights = len(target_list) / \
-        torch.tensor(class_count, dtype=torch.float)
-    class_weights = class_weights / len(class_weights)
-    return class_weights.tolist()
+    n, n_classes = target_list.shape
+    weights = float(n) / target_list.sum(axis=0)
+    weights /= n_classes  # 3 typically
+
+    return weights
 
 
 # TODO: Use the learning rate
@@ -319,28 +318,31 @@ def bluebert_create_train_model(multi_nli_train_x: np.ndarray,
     if use_multi_nli:
         multinli_x_train_dataset = ContraDataset(list(multi_nli_train_x), multi_nli_train_y, tokenizer, max_len=512,
                                                  multi_class=multi_class)
-        multinli_x_train_sampler = WeightedRandomSampler(get_class_weights(list(multi_nli_train_y)), multinli_x_train_dataset)
+        multinli_x_train_sampler = WeightedRandomSampler(get_class_weights(multi_nli_train_y), len(multi_nli_train_x))
         multinli_x_train_dataloader = DataLoader(multinli_x_train_dataset,
                                                  sampler=multinli_x_train_sampler, batch_size=batch_size)
 
     if use_med_nli:
         mednli_x_train_dataset = ContraDataset(list(med_nli_train_x), med_nli_train_y, tokenizer, max_len=512,
                                                multi_class=multi_class)
-        mednli_x_train_sampler = WeightedRandomSampler(get_class_weights(list(med_nli_train_y)), mednli_x_train_dataset)
+        mednli_x_train_sampler = WeightedRandomSampler(get_class_weights(med_nli_train_y), len(med_nli_train_x))
         mednli_x_train_dataloader = DataLoader(mednli_x_train_dataset, sampler=mednli_x_train_sampler,
                                                batch_size=batch_size)
 
     if use_man_con:
         mancon_x_train_dataset = ContraDataset(list(man_con_train_x), man_con_train_y, tokenizer, max_len=512,
                                                multi_class=multi_class)
-        mancon_x_train_sampler = WeightedRandomSampler(get_class_weights(list(man_con_train_y)), mancon_x_train_dataset)
+        ## print(man_con_train_y)
+        mancon_x_train_sampler = WeightedRandomSampler(get_class_weights(man_con_train_y), len(man_con_train_x))
         mancon_x_train_dataloader = DataLoader(mancon_x_train_dataset, sampler=mancon_x_train_sampler,
                                                batch_size=batch_size)
 
     if use_cord:
         cord_x_train_dataset = ContraDataset(list(cord_train_x), cord_train_y, tokenizer, max_len=512,
                                              multi_class=multi_class)
-        cord_x_train_sampler = WeightedRandomSampler(get_class_weights(list(cord_train_y)), cord_x_train_dataset)
+        ## print(cord_train_y)
+        print(get_class_weights(cord_train_y))
+        cord_x_train_sampler = WeightedRandomSampler(get_class_weights(cord_train_y), len(cord_train_x))
         cord_x_train_dataloader = DataLoader(cord_x_train_dataset, sampler=cord_x_train_sampler, batch_size=batch_size)
 
     losses_list = []

src/contradictory_claims/models/sbert_models.py
@@ -227,8 +227,11 @@ def trainer(model: SBERTPredictor,
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     criterion = nn.CrossEntropyLoss(weight=class_weights.to(device))
+
     # NOTE: using gradient clipping
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate, clipvalue=.5)
+    for p in model.parameters():
+        p.register_hook(lambda grad: torch.clamp(grad, -1.0, 1.0))
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     model.to(device)
 
     print("------TRAINING STARTS----------")  # noqa: T001
@@ -329,7 +332,8 @@ def build_sbert_model(model_name: str, logistic_model: bool = True):
         model_name = "allenai/biomed_roberta_base"
         ri = str(randrange(100000))
         # ToDo: Need to not hard-code these paths
-        model_save_path = f"/scratch/users/dnsosa/biobert_models/biobert_RI{ri}"
+        ###model_save_path = f"/scratch/users/dnsosa/biobert_models/biobert_RI{ri}"
+        model_save_path = f"output/trained_bluebert/jkbiobert{ri}"
         if not os.path.exists(model_save_path):
             os.makedirs(model_save_path, exist_ok=True)
         wget.download(

src/contradictory_claims/models/train_model.py
@@ -86,11 +86,11 @@ def build_model(transformer, max_len: int = 512, multi_class: bool = True, init_
 
     if multi_class:
         # NOTE: adding in gradient clipping
-        model.compile(Adam(learning_rate=lr, clipvalue=0.5), loss='categorical_crossentropy',
+        model.compile(Adam(learning_rate=lr, clipvalue=1.0), loss='categorical_crossentropy',
                       metrics=[tf.keras.metrics.Recall(), tf.keras.metrics.Precision(),
                                tf.keras.metrics.CategoricalAccuracy()])
     else:
-        model.compile(Adam(learning_rate=lr, clipvalue=0.5), loss='binary_crossentropy',
+        model.compile(Adam(learning_rate=lr, clipvalue=1.0), loss='binary_crossentropy',
                       metrics=[tf.keras.metrics.Recall(), tf.keras.metrics.Precision(), 'accuracy'])
 
     return model
@@ -140,17 +140,28 @@ def load_model(pickle_path: str, transformer_dir: str = 'transformer', max_len: 
     return model
 
 
-def get_class_weights(target_list: list):
+# def get_class_weights(target_list: list):
+#    """Return the class weights to tackle skewness in data while training.
+#     :param target_list: list indicating class membership for calculating imbalance
+#     :return: list of weights
+#     """
+#     print(target_list)
+#     count_dict = Counter(target_list)
+#     class_count = [count_dict[i] for i in range(3)]
+#     class_weights = len(target_list) / \
+#         torch.tensor(class_count, dtype=torch.float)
+#     class_weights = class_weights / len(class_weights)
+#     return class_weights.tolist()
+
+def get_class_weights(target_list: np.array):
     """Return the class weights to tackle skewness in data while training.
-    :param target_list: list indicating class membership for calculating imbalance
+    :param target_list: numpy array list indicating binary-encoded class membership for calculating imbalance
     :return: list of weights
     """
-    count_dict = Counter(target_list)
-    class_count = [count_dict[i] for i in range(3)]
-    class_weights = len(target_list) / \
-        torch.tensor(class_count, dtype=torch.float)
-    class_weights = class_weights / len(class_weights)
-    return class_weights.tolist()
+    n, n_classes = target_list.shape
+    weights = float(n) / target_list.sum(axis=0)
+    weights /= n_classes  # 3 typically
+    return weights
 
 
 def train_model(multi_nli_train_x: np.ndarray,
@@ -335,7 +346,7 @@ def train_model(multi_nli_train_x: np.ndarray,
                                   validation_data=(multi_nli_test_x, multi_nli_test_y),
                                   callbacks=[es, WandbCallback()],
                                   epochs=epochs,
-                                  class_weight=get_class_weights(list(multi_nli_train_y)))
+                                  class_weight=get_class_weights(multi_nli_train_y))
         train_hist_list.append(train_history)
 
         print("passed the multiNLI train. Now the history:")  # noqa: T001
@@ -349,7 +360,7 @@ def train_model(multi_nli_train_x: np.ndarray,
                                   validation_data=(med_nli_test_x, med_nli_test_y),
                                   callbacks=[es, WandbCallback()],
                                   epochs=epochs,
-                                  class_weight=get_class_weights(list(med_nli_train_y)))
+                                  class_weight=get_class_weights(med_nli_train_y))
         train_hist_list.append(train_history)
 
     # Fine tune on ManConCorpus
@@ -360,7 +371,7 @@ def train_model(multi_nli_train_x: np.ndarray,
                                   validation_data=(man_con_test_x, man_con_test_y),
                                   callbacks=[es, WandbCallback()],
                                   epochs=epochs,
-                                  class_weight=get_class_weights(list(man_con_train_y)))
+                                  class_weight=get_class_weights(man_con_train_y))
         train_hist_list.append(train_history)
 
     # Fine tune on CORD-19
@@ -371,7 +382,7 @@ def train_model(multi_nli_train_x: np.ndarray,
                                   validation_data=(cord_test_x, cord_test_y),
                                   callbacks=[es, WandbCallback()],
                                   epochs=epochs,
-                                  class_weight=get_class_weights(list(cord_train_y)))
+                                  class_weight=get_class_weights(cord_train_y))
         train_hist_list.append(train_history)
 
     return model, train_hist_list